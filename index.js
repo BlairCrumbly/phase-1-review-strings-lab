d1a2461a0db6c9a381acea69ed16a0e828a86ba2
Blair Crumbly <blaircrumbly@gmail.com>
2025-01-06 22:23:48 -0500

    Made 3 welcomes, Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "Blair";
+let firstLetter = currentUser[0];
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toLocaleUpperCase() + "!";
+
+
+let shortGreeting = `Welcome, ${firstLetter}!`
